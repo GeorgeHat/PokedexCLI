76e01101bf67ca7b7c6810443adea715f4e3d9a8
GeorgeHat <georgehat2003@gmail.com>
2026-07-02 04:03:30 +0300

    Added exit and help commands

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	allowedCommands := getCommands()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		input := scanner.Text()
+		cleanedInput := cleanInput(input)
+		firstWord := cleanedInput[0]
+		command, ok := allowedCommands[firstWord]
+		if !ok {
+			println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Printf("Error excecuting command: %v: %v", command.name, err)
+		}
+
+	}
 
 }
